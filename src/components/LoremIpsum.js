ab55ce20fbf775ed9bd29897fe1d9cdd9ed9e1d2
Vinícius Souza Girardi <vini.girardi14@gmail.com>
2021-08-03 20:29:43 -0300

    first commit

---
+++ src/components/LoremIpsum.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoremIpsum = () => {
+    return <p>Eu espero aprender muito React</p>
+}
+
+export default LoremIpsum;
---
+++ src/components/planets/planet/index.js
@@ -0,0 +1,43 @@
+import React, {Fragment, useState, useEffect} from 'react'
+import GrayImg from '../../shared/gray_img'
+import DescriptionWithLink from '../../shared/description_with_link'
+
+async function getSatellites(id){
+    let response = await fetch(`http://localhost:3000/api/${id}.json`)
+    let data = await response.json();
+    return data
+}
+
+const Planet = (props) => {
+    const [satellites, setSatellites] = useState([]);
+    
+    useEffect(() => {
+        getSatellites(props.id).then(data => {
+            setSatellites(data['satellites'])
+        })
+    }, [])
+
+        let title1;
+        if(props.title_with_underline)
+            title1 = <h4><u>{props.name}</u></h4>
+        else
+            title1 = <h4>{props.name}</h4>
+
+        return (
+            <div>
+                {title1}
+                <DescriptionWithLink description={props.description} link={props.link}/>
+                <GrayImg img_url={props.img_url} gray={props.gray}/>
+                <h4>Satélites</h4>
+                <ul>
+                    {satellites.map((satellite, index) =>
+                        <li key={index}>{satellite.name}</li>
+                    )}
+                </ul>
+                <hr/>
+            </div>   
+        )
+}
+
+
+export default Planet;
